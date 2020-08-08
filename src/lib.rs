1de33a77329f9a55059a0cdfaebcc97d70c02b7d
Ryo Ota <nwtgck@nwtgck.org>
2020-08-09 03:59:27 +0900

    refactor: js_sys::Array::from_iter()

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use js_sys;
+use std::iter::FromIterator;
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,20 +9,12 @@ extern "C" {
     pub fn log() -> js_sys::Function;
 }
 
-pub fn js_sys_array<I: IntoIterator<Item=T>, T: Into<JsValue>>(into_iter: I) -> js_sys::Array {
-    let js_arr = js_sys::Array::new();
-    into_iter.into_iter().for_each(|x| {
-        js_arr.push(&x.into());
-    });
-    js_arr
-}
-
 #[macro_export]
 macro_rules! log {
     ( $( $x:expr ),* ) => {
         $crate::Console::log().apply(
             &JsValue::null(),
-            &$crate::js_sys_array(&[$(
+            &$crate::js_sys::Array::from_iter(&[$(
                 Into::<wasm_bindgen::JsValue>::into($x),
             )*])
         )
