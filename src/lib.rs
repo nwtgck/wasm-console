93e6104e7913e80cc222fdb28f143c3a93031047
Ryo Ota <nwtgck@nwtgck.org>
2020-08-08 22:08:25 +0900

    feat: implement log!() macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen::prelude::*;
+use js_sys;
+
+#[wasm_bindgen(inline_js = "export function console_log() { return console.log; }")]
+extern "C" {
+    fn console_log() -> js_sys::Function;
+}
+
+fn js_sys_array<I: IntoIterator<Item=T>, T: Into<JsValue>>(into_iter: I) -> js_sys::Array {
+    let js_arr = js_sys::Array::new();
+    into_iter.into_iter().for_each(|x| {
+        js_arr.push(&x.into());
+    });
+    js_arr
+}
+
+#[macro_export]
+macro_rules! log {
+    ( $( $x:expr ),* ) => {
+        console_log().apply(
+            &JsValue::null(),
+            &js_sys_array(&[$(
+                Into::<wasm_bindgen::JsValue>::into($x),
+            )*])
+        )
+    };
 }
